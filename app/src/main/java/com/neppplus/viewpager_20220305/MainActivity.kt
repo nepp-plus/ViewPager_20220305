0a43238bce4c6efd2464e6999ad928e3516ce600
nepp.plus <nepp.plus@gmail.com>
2022-03-05 14:53:57 +0900

    offscreenPageLimit 설정

--- app/src/main/java/com/neppplus/viewpager_20220305/MainActivity.kt
+++ app/src/main/java/com/neppplus/viewpager_20220305/MainActivity.kt
@@ -25,6 +25,12 @@ class MainActivity : AppCompatActivity() {
         mAdapter = MainViewPagerAdapter( supportFragmentManager )
         mainViewPager.adapter = mAdapter
 
+//        뷰페이저도 재사용성이 적용됨. 2~3장의 화면을 가지고, 프래그먼트를 상황에 따라 새로 만들거나
+//        함수 추가 호출 하는 등의 동작.
+
+//        3장 or 4장 등 원하는 페이지수 전체를 메모리에 유지하게 하는 명령어.
+        mainViewPager.offscreenPageLimit = 3
+
 
     }
 
