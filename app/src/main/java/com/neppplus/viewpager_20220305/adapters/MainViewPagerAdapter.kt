d202e00851570c4331fb9007a70ed4d93c12ea2f
nepp.plus <nepp.plus@gmail.com>
2022-03-05 15:07:29 +0900

    각 페이지별 제목 리턴  - 뷰페이져 어댑터에 추가

--- app/src/main/java/com/neppplus/viewpager_20220305/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/com/neppplus/viewpager_20220305/adapters/MainViewPagerAdapter.kt
@@ -9,6 +9,20 @@ import com.neppplus.viewpager_20220305.fragments.MyNameFragment
 
 class MainViewPagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm) {
 
+    override fun getPageTitle(position: Int): CharSequence? {
+
+        if (position == 0) {
+            return "이름"
+        }
+        else if (position == 1) {
+            return "내 정보"
+        }
+        else {
+            return "인사말"
+        }
+
+    }
+
     override fun getCount(): Int {
 
         return 3
