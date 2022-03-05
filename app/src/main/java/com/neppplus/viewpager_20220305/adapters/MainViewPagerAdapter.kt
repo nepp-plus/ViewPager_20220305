fb183278ad01cf1d19296f4b6635325681026fea
nepp.plus <nepp.plus@gmail.com>
2022-03-05 14:46:24 +0900

    뷰페이져 어댑터의 실제 기능 구현

--- app/src/main/java/com/neppplus/viewpager_20220305/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/com/neppplus/viewpager_20220305/adapters/MainViewPagerAdapter.kt
@@ -3,14 +3,29 @@ package com.neppplus.viewpager_20220305.adapters
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import com.neppplus.viewpager_20220305.fragments.HelloFragment
+import com.neppplus.viewpager_20220305.fragments.MyInfoFragment
+import com.neppplus.viewpager_20220305.fragments.MyNameFragment
 
 class MainViewPagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm) {
 
     override fun getCount(): Int {
 
+        return 3
+
     }
 
     override fun getItem(position: Int): Fragment {
+
+        if (position == 0) {
+            return MyNameFragment()
+        }
+        else if (position == 1) {
+            return MyInfoFragment()
+        }
+        else {
+            return HelloFragment()
+        }
 
     }
 }
