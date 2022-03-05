417ca1e2e4e77f71503e2817488ef13067509e50
nepp.plus <nepp.plus@gmail.com>
2022-03-05 15:37:59 +0900

    페이지 제목도 return when 처리

--- app/src/main/java/com/neppplus/viewpager_20220305/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/com/neppplus/viewpager_20220305/adapters/MainViewPagerAdapter.kt
@@ -11,14 +11,10 @@ class MainViewPagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm) {
 
     override fun getPageTitle(position: Int): CharSequence? {
 
-        if (position == 0) {
-            return "이름"
-        }
-        else if (position == 1) {
-            return "내 정보"
-        }
-        else {
-            return "인사말"
+        return when(position) {
+            0 -> "이름"
+            1 -> "내 정보"
+            else -> "인사말"
         }
 
     }
